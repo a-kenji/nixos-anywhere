e629da8213e0ba1337424738ca0722660c69ef03
a-kenji <aks.kenji@protonmail.com>
2025-07-24 09:33:54 +0200

    WIP: fix variable expansion

--- src/nixos-anywhere.sh
+++ src/nixos-anywhere.sh
@@ -587,8 +587,7 @@ generateHardwareConfig() {
     if [[ ${hasNixOSFacter} == "n" ]]; then
       step "Generating facter.json using nixos-facter from nixpkgs"
       runSshNoTty -o ConnectTimeout=10 "${maybeSudo}" \
-        nix run "${nixOptions[@]}" \
-        nixpkgs#nixos-facter >"$hardwareConfigPath"
+        "nix run ${nixOptions[*]} nixpkgs#nixos-facter" >"$hardwareConfigPath"
     else
       step "Generating facter.json using nixos-facter"
       runSshNoTty -o ConnectTimeout=10 "${maybeSudo}" "nixos-facter" >"$hardwareConfigPath"
